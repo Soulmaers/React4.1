afcd9238d57caac69b27da4fe86616f5546d94d1
Soulmaers <certiss@ya.ru>
2022-08-19 14:54:59 +0300

    готово

--- src/App.js
+++ src/App.js
@@ -1,35 +1,26 @@
 import React from 'react'
 import './App.css';
-import { useState } from 'react'
+
 import Form from './components/Form.js'
 
 function App() {
 
+  const color = {
+    hex: '',
+    rgb: ''
+  }
 
   function hexToRGB(hex) {
-    hex = '0x' + hex
-    let r = (hex >> 16) & 0xFF
-    let g = (hex >> 8) & 0xFF
-    let b = hex & 0xFF
-    return `rgb(${r}, ${g}, ${b})`
+    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+    return result ? {
+      r: parseInt(result[1], 16),
+      g: parseInt(result[2], 16),
+      b: parseInt(result[3], 16)
+    } : null;
   }
 
-  const [form, setForm] = useState({
-    input: '',
-    rgb: ''
-  })
-  const updateColor = event => {
-    setForm(() => ({
-      input: event.trim(),
-      rgb: event.length === 7 ? hexToRGB(event) : ''
-    }))
+  return <> <Form color={color} hexToRGB={hexToRGB} /></>
 
-  }
-  return (
-    <div className="container" style={{ background: form.rgb }} >
-      <Form onChange={updateColor} value={form.input} rgb={form.rgb} />
-    </ div>
-  );
 }
 
 export default App;
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,21 +1,27 @@
+import React from 'react'
+import { useState } from "react";
 
-export default function Form({ onChange, value, rgb }) {
 
+export default function Form({ color, hexToRGB }) {
+    const [input, setInput] = useState(color);
     const onChangeForm = (event) => {
-        onChange(event.target.value)
+        const { value } = event.target;
+        if (value.length < 7) {
+            setInput((prev) => ({ ...prev, hex: value }));
+            return;
+        }
+        const rgbColor = hexToRGB(value);
+        setInput(prev => ({
+            ...prev,
+            hex: value,
+            rgb: rgbColor === null ? 'Ошибка' : `rgb(${rgbColor.r},${rgbColor.g},${rgbColor.b})`
+        }))
     }
     return (
-        <>
-            <form className="form">
-                <input
-                    className="input"
-                    type="text"
-                    maxLength="7"
-                    onChange={onChangeForm}
-                    value={value}>
-                </input>
-            </form>
-            <div className="rgbOut">{rgb}</div>
-        </>
+        <div className="container" style={{ background: input.rgb }} >
+            <input className="input" maxLength='7' onChange={onChangeForm} value={input.hex} />
+            <div className="rgbOut" style={{ background: input.rgb }}> {input.rgb}  </div>
+        </div>
+
     )
 }
